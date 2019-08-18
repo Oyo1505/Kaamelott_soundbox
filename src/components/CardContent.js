ff44972481e2cdf0974899f7c558e4204f124f9e
RIGOULET Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2019-08-18 11:12:50 +0200

    clean up

--- src/components/CardContent.js
+++ src/components/CardContent.js
@@ -1,63 +1,58 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 class CardContent extends Component {
 
-	
-	constructor(props){
-		super(props);
-		 this.state = {
+
+    constructor(props) {
+        super(props);
+        this.state = {
             active: false,
         };
-		this.play = this.play.bind(this)
-		this.soundEnded = this.soundEnded.bind(this)
-	}
+        this.play = this.play.bind(this)
+        this.soundEnded = this.soundEnded.bind(this)
+    }
 
-	replaceString(title){
-		
-		let newTitle = `${title.substr(0, 27)}...`;
-	
-		return newTitle;
+    replaceString(title) {
 
-	}
-	play(){
-			
-			var sound = document.getElementById(`${this.props.data.file}`);
-			const currentState = this.state.active;
-			sound.currentTime=0;
-			if(!currentState){
-				sound.play();
-			}else if(currentState){
-				sound.pause();
-			}
-        	this.setState({ active: !currentState });
-			
-			
-				
+        let newTitle = `${title.substr(0, 27)}...`;
 
-			
+        return newTitle;
 
+    }
+    
+    play() {
 
-	}
-
-	soundEnded(){
-		var sound = document.getElementById(`${this.props.data.file}`)
-		const currentState = this.state.active;
+        var sound = document.getElementById(`${this.props.data.file}`);
+        const currentState = this.state.active;
+        sound.currentTime = 0;
+        if (!currentState) {
+            sound.play();
+        } else if (currentState) {
+            sound.pause();
+        }
         this.setState({ active: !currentState });
-		if (sound !== undefined) {
-		    sound.pause();
-		  }
-	}	
+    }
 
-	render(){
+    soundEnded() {
+        var sound = document.getElementById(`${this.props.data.file}`)
+        const currentState = this.state.active;
+        this.setState({ active: !currentState });
+        if (sound !== undefined) {
+            sound.pause();
+        }
+    }
 
-			let title = this.props.data.title;
-			let playingClass = ['playing'];
+    render() {
+
+        let title = this.props.data.title;
+        let playingClass = ['playing'];
 
 
-			const soundFile = require(`../sounds/kamelott/${this.props.data.file}`);
-		
-		 return (
-		    <Fragment>
+        const soundFile = require(`../../sounds/kamelott/${this.props.data.file}`);
+
+        return (
+            <Fragment>
 		    	
 		    		<li key={`sound-${title}`} className="sound-item"> <button onClick={this.play} onEnded={this.soundEnded} ><audio className={this.state.active ? 'playing' : null} preload="none" id={this.props.data.file}   src={soundFile}></audio><span>{title.lenght <= 27  ? 
 
@@ -65,13 +60,16 @@ class CardContent extends Component {
 
 		    			this.replaceString(title)
 
-		    			}</span>  <i className={this.state.active ? 'icon icon-pause' :'icon icon-play' }></i> </button></li>
+		    			}</span>  <i className={this.state.active ? 'icon icon-pause icon-play-soundbox ' :'icon icon-play icon-play-soundbox ' }></i> </button></li>
 		    	
 		    </Fragment>
-		  )
-	}
- 
+        )
+    }
+
 }
 
 export default CardContent;
 
+CardContent.propTypes = {
+    active: PropTypes.bool,
+}
--- src/components/SoundCard.js
+++ src/components/SoundCard.js
@@ -1,35 +1,11 @@
 import React, { Component, Fragment } from 'react';
-import { Col,Button } from 'react-bootstrap';
-import  Modal from 'react-bootstrap/lib/Modal';
+import Modal from 'react-bootstrap/Modal';
 import styled from 'styled-components';
-import {  animated } from 'react-spring';
-import { Card} from '../Elements';
-import { Toggle, absolute } from '../Utilities';
 import CardContent from './CardContent';
-//import Icon from '../Elements/Icon';
 
-const sounds = require('../sounds/kamelott/sounds.json');
-let rand = () => Math.floor(Math.random() * 20) - 10;
+const sounds = require('../../sounds/kamelott/sounds.json');
 
-//const AnimCard = Card.withComponent(animated.div);
-const modalStyle = function() {
-  // we use some psuedo random coords so nested modals
-  // don't sit right on top of each other.
-  let top = 50 + rand();
-  let left = 50 + rand();
 
-  return {
-    position: 'fixed',
-    width: 400,
-    zIndex: 1040,
-    top: 25 +'%',
-    left: 25+'%',
-    border: '1px solid #e5e5e5',
-    backgroundColor: 'white',
-    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
-    padding: 20
-  };
-};
 
 const CardTest = styled.div`
 	width: 100%;
@@ -44,17 +20,10 @@ const CardTest = styled.div`
 		box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 	}
 	&.active{
-		
+
 	}
 `;
-const CardContainer = styled(animated.div)`
-	position: relative;
-	width:200px;
-	height:300px;
-	max-width: 35px;
-	margin: 0 auto;
-	border-radius:5px;
-`;
+
 
 const backdropStyle = {
   position: 'fixed',
@@ -84,13 +53,13 @@ class SoundCard extends Component {
 	toggle(){
 
 		this.setState({end: !this.state.end});
-	} 
+	}
 
 	  renderBackdrop(props) {
 	    return <div {...props} style={backdropStyle} />;
 	  }
 	render() {
-		const { toggle , on } = this.props;
+	
 		const isEnd = this.state.end;
 		let boxClass = ["box"];
 
@@ -99,72 +68,58 @@ class SoundCard extends Component {
 			boxClass.push('active');
 		}
 
-
-		//console.log(this.props.kaamelott)
-		 			
 		return(
-			
-						
+
+
 						<CardTest className={boxClass.join(' ')} id={this.props.kaamelott.character}   >
-					
-									
-							<img className="picture-character" id={`picture-${this.props.kaamelott.character}`} src={this.props.kaamelott.file} />
+
+
+							<img className="picture-character" id={`picture-${this.props.kaamelott.character}`} src={this.props.kaamelott.file} alt={`picture-${this.props.kaamelott.character}`}/>
 							<h4 className="title-character">{this.props.kaamelott.character  }</h4>
-						
-					   		
-								
-									 
-									<>
+
+
+
+
+									<Fragment>
 					                <button className='btnn btn-2' onClick={this.toggle}>Ouvrir</button>
-					                
+
 					                <Modal  show={this.state.end} onHide={this.handleClose}
 					                size="lg"
-					              	
+
 							        aria-labelledby="contained-modal-title-vcenter"
 							        centered>
-							    
+
 					                          <Modal.Header closeButton>
-					                          
-									            <Modal.Title> 
-									            <img className="picture-character-modal" src={this.props.kaamelott.file} id={`picture-${this.props.kaamelott.character}`}  />
+
+									            <Modal.Title>
+									            <img className="picture-character-modal" src={this.props.kaamelott.file} id={`picture-card-content-${this.props.kaamelott.character}`} alt={`picture-${this.props.kaamelott.character}`} />
 									           <h3 className="title-character-modal">{this.props.kaamelott.character  }</h3></Modal.Title>
 									          </Modal.Header>
 									          <Modal.Body>
-					               
+
 					               				 <ul className="sound-list">
 													{sounds.map(sound =>{
 																	if(this.props.kaamelott.character === sound.character){
 																		return(<CardContent  key={`card-content-${this.props.kaamelott.character}-${sound.title}`}  data={sound} file={sound.file} />);
-																	}			
-														}	
-											)} 				
-								
-									</ul> 
+																	}
+														}
+											)}
+
+									</ul>
 									</Modal.Body>
-									
+
 									</Modal>
-					                </>
-					             
-									
-									
+					                </Fragment>
+
+
+
 						</CardTest>
 
-					
 
-			
-		
+
+
+
 			)
 	}
 }
-
-const CloseButton = styled.button`
-	${absolute({
-		y :'top',
-		x : 'right'
-	})};
-	border:none;
-	background:transparent;
-	padding: 10px;
-`;
-
 export default SoundCard;
